d6f979157f2b2ba3549d181680aee9a74b1a0d3b
jua jua <juajuajuag@gmail.com>
2026-09-19 17:34:21 -0500

    Preparar punto de entrada externo

--- init.lua
+++ init.lua
@@ -1,12 +1,14 @@
 local Library={}
-local Loader=require("Loader.Loader")
 
 Library.Name="LuaLibrary"
 Library.Version="1.0.0"
-Library.Loader=Loader
 
 Library.Modules={
  Dashboard="Modules/Dashboard.lua"
 }
+
+function Library:GetModule(Name)
+ return self.Modules[Name]
+end
 
 return Library
