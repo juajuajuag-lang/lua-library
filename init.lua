ab6bf50d0466d946dc8dce56243d06b03a546349
jua jua <juajuajuag@gmail.com>
2026-09-19 17:02:24 -0500

    Preparar punto de entrada de la libreria

--- init.lua
+++ init.lua
@@ -1,11 +1,8 @@
 local Library={}
-local Registry=require("Core.Registry")
-local Dashboard=require("Modules.Dashboard")
 
 Library.Name="LuaLibrary"
 Library.Version="1.0.0"
 
-Library.Registry=Registry
-Library.Dashboard=Registry:Register("Dashboard",Dashboard)
+Library.Modules={}
 
 return Library
